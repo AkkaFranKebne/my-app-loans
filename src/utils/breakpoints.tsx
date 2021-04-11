c81e0c5a5feaea91d565c070c22d09a0ab06c438
AkkaFranKebne <ewawrobel@gmail.com>
2021-04-11 19:00:57 +0200

    css reset and breakpoints

---
+++ src/utils/breakpoints.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Media from 'react-media';
+
+export const OnMobileOnly: React.FC = ({ children }) => {
+  return <Media query="(max-width: 640px)" defaultMatches={false} render={() => children} />;
+};
+
+export const OnTabletOnly: React.FC = ({ children }) => {
+    return <Media query="(min-width: 801px) and (max-width: 991px)" defaultMatches={false} render={() => children} />;
+  };
+
+export const OnDesktopOnly: React.FC = ({ children }) => {
+  return <Media query="(min-width:992px)" defaultMatches={false} render={() => children} />;
+};
+
