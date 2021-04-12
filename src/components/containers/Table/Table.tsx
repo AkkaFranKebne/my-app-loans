5dadd5e0691b27a5f1b977ea280b7b90fceb9592
AkkaFranKebne <ewawrobel@gmail.com>
2021-04-12 22:37:38 +0200

    Table

--- src/components/containers/Table/Table.tsx
+++ src/components/containers/Table/Table.tsx
@@ -1,26 +1,47 @@
-import React, { FunctionComponent, useEffect } from 'react';
+import React, { FunctionComponent, useState } from 'react';
 
 interface OwnProps {
   data: any;
 }
 
-type TableProps = Omit<JSX.IntrinsicElements['div'], 'children'> & OwnProps;
+type TableProps = Omit<JSX.IntrinsicElements['table'], 'children'> & OwnProps;
 
 const Table: FunctionComponent<TableProps> = props => {
+    const [loan, setLoan] = useState(0);
+    const [fee, setFee] = useState(0);
+    const [apr, setApr] = useState(0);
+
     const { data } = props;
+
     return (
-        <div>
+        <table width="100%">
+              {data?.data?.length > 0 &&(
+                <tr>
+                    <th>creditor</th>
+                    <th>loan</th>
+                    <th>fee</th>
+                    <th>APR</th>
+                </tr>
+            )}
             {data?.data?.map((item: any) => {
                 return (
-                    <div>
-                        creditor: {item.creditor} 
-                        loan: {item.loan} 
-                        fee: {item.fee} 
-                        apr: {item.APR} 
-                    </div>
+                    <tr>
+                        <td>{item.creditor} </td>
+                        <td>{item.loan} </td>
+                        <td>{item.fee} </td>
+                        <td> {item.APR} </td>
+                    </tr>
                 )
             })}
-            </div>
+            {data?.data?.length > 0 &&(
+                <tr>
+                    <th>-</th>
+                    <th>{loan}</th>
+                    <th>{fee}</th>
+                    <th>{apr}</th>
+                </tr>
+            )}
+        </table>
     );
 };
 
