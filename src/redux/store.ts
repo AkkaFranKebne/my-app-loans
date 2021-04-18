04044b7146a86d0ea87a1659012d71f40d3fa09c
AkkaFranKebne <ewawrobel@gmail.com>
2021-04-18 13:22:20 +0200

    redux types

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,8 +1,17 @@
-import { createStore } from 'redux';
+import { createStore, Store } from 'redux';
 import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 import rootReducer from './reducer';
 import hardSet from 'redux-persist/lib/stateReconciler/hardSet';
+import { AddTestAction, AddTotalsAction } from '../redux/actionTypes';
+import { }
+
+interface IAppState {
+  test?: IAuthenticationState;
+  totals: {
+    loading: boolean;
+  };
+}
 
 const persistConfig = {
   key: 'root',
@@ -13,13 +22,12 @@ const persistConfig = {
 //@ts-ignore
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-const store = createStore(
+const store: Store<PersistPartial, AddTestAction | AddTotalsAction> = createStore(
   persistedReducer,
   //@ts-ignore
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
 );
 
-//@ts-ignore
 const persistor = persistStore(store);
 
 export default { store, persistor };
